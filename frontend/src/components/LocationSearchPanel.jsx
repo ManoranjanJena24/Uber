283dea751cce3a41a6d07447fd25eae51307518e
unknown <manoranjanjenamitsy17@gmail.com>
2024-12-08 16:59:35 +0530

    Created the structure modal for choose type of ride

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const LocationSearchPanel = () => {
   return (
     <div>
-        <div className='flex gap-4 items-center my-4 justify-start'>
+        <div className='flex gap-4 active:border-2  border-gray-100  active:border-black  p-3 rounded-xl items-center my-2 justify-start'>
             <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
             <h4 className='font-medium'>24B, Near Iranian Cafe ,Phoeneix market,Pune</h4>
         </div>
 
-        <div className='flex gap-4 items-center my-4 justify-start'>
+        <div className='flex gap-4 active:border-2 p-3 border-white active:border-black  rounded-xl items-center my-2 justify-start'>
             <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
             <h4 className='font-medium'>24B, Near Iranian Cafe ,Phoeneix market,Pune</h4>
         </div>

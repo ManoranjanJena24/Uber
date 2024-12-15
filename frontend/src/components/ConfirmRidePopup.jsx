f2d8f4543cf3d52b0258c5f0f03cd93502883d4a
unknown <manoranjanjenamitsy17@gmail.com>
2024-12-15 13:21:58 +0530

    Made till Finish Ride Panel in Captain Side

---
+++ frontend/src/components/ConfirmRidePopup.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ConfirmRidePopup = (props) => {
+    return (
+        <div>
+            <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
+                props.setRidePopupPanel(false)
+            }}><i className=" text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
+
+            <h3 className='text-2xl font-semibold mb-5'>Confirm this ride to start</h3>
+            <div className='flex items-center justify-between p-3 bg-yellow-400 rounded-lg mt-4'>
+                <div className='flex items-center gap-3 '>
+                    <img className='h-12 w-12 rounded-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROoe69egKDrVYj71IllAQW7XAJ3lolFI-9vw&s" alt="" />
+                    <h2 className='text-lg font-medium'>Virat Kholi</h2>
+                </div>
+                <h5 className='text-lg font-semibold '>2.2 KM</h5>
+            </div>
+            <div className='flex gap-2 justify-between flex-col items-center'>
+                <div className='w-full mt-5'>
+                    <div className='flex items-center gap-5 p-3 border-b-2'>
+                        <i className="ri-map-pin-user-fill"></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>562/11-A</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>Kankariya Talab, Bhopal</p>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <div className='flex items-center gap-5 p-3 border-b-2'>
+                        <i className="text-lg ri-map-pin-2-fill"></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>562/11-A</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>Kankariya Talab, Bhopal</p>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+                    <div className='flex items-center gap-5 p-3'>
+                        <i className="ri-currency-fill"></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>₹193.20</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>Cash Cash </p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div className='mt-6 w-full'>
+                    <form onSubmit={(e)=>{
+                        submitHandler(e)
+                    }} >
+
+                        <input type="text"  className='bg-[#eee] px-6 py-4  font-mono text-lg rounded-lg w-full mt-3' placeholder='Enter OTP' />
+
+                        <Link to='/captain-riding' className='w-full mt-5 bg-green-600 flex justify-center text-white font-semibold p-3 rounded-lg'> Confirm</Link>
+
+                        <button onClick={() => {
+                            props.setConfirmRidePopupPanel(false)
+                            props.setRidePopupPanel(false)
+
+                        }} className='w-full mt-2 bg-red-600 text-white font-semibold p-3 rounded-lg'> Cancel</button>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ConfirmRidePopup
--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,53 +1,66 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import CaptainDetails from '../components/CaptainDetails'
+import RidePopUp from '../components/RidePopUp'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import ConfirmRidePopup from '../components/ConfirmRidePopup'
 
 const CaptainHome = () => {
+  const [ridePopupPanel, setRidePopupPanel] = useState(true)
+  const [confirmRidePopupPanel, setConfirmRidePopupPanel] = useState(false)
+  const ridePopupPanelRef = useRef(null)
+  const confirmRidePopupPanelRef = useRef(null)
+
+  useGSAP(function(){
+    if(ridePopupPanel){
+      gsap.to(ridePopupPanelRef.current,{
+        transform:'translateY(0)'
+      })
+    }
+    else{
+      gsap.to(ridePopupPanelRef.current,{
+        transform:'translateY(100%)'
+      })
+    }
+  },[ridePopupPanel])
+
+  useGSAP(function(){
+    if(confirmRidePopupPanel){
+      gsap.to(confirmRidePopupPanelRef.current,{
+        transform:'translateY(0)'
+      })
+    }
+    else{
+      gsap.to(confirmRidePopupPanelRef.current,{
+        transform:'translateY(100%)'
+      })
+    }
+  },[confirmRidePopupPanel])
+
   return (
     <div className='h-screen'>
       <div className='fixed p-6 top-0 flex item-center justify-between w-screen'>
         <img className='w-16' src="https://th.bing.com/th/id/R.eadac11b662faa57e4b806263f8e7642?rik=dXbhLg7x49QVbg&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fuber-logo-vector-png-uber-icon-png-50-px-1600.png&ehk=hWY6TYYwZAYpPbHKLid%2f431JJx%2frKI7tiY%2b6i993Y1A%3d&risl=&pid=ImgRaw&r=0" alt="" />
-        <Link to='/home' className='h-10 w-10 bg-white flex items-center justify-center rounded-full'>
-        <i className=" text-lg font-medium ri-logout-box-r-line"></i>
-      </Link>
+        <Link to='/captain-home' className='h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+          <i className=" text-lg font-medium ri-logout-box-r-line"></i>
+        </Link>
       </div>
       <div className='h-3/5'>
         <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
 
       </div>
       <div className='h-2/5 p-6'>
-      <div className='flex items-center justify-between'>
-      <div className='flex items-center justify-start gap-3'>
-          <img className='h-10 w-10 rounded-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLu9civCRYbYXqaw5Ao3z96nASbtphI0YtZA&s" alt="" />
-          <h4 className='text-lg font-medium'>Harsh Sharma</h4>
-        </div>
-        <div>
-          <h4 className='text-xl font-semibold'>₹295.20</h4>
+        <CaptainDetails />
 
-          <p className='text-sm  text-gray-600'>Earned</p>
-        </div>
+      </div>
+      <div ref={ridePopupPanelRef} className='fixed w-full z-10 bottom-0 -translate-y-full  bg-white  px-3 py-6 pt-12'>
+        <RidePopUp  setRidePopupPanel={setRidePopupPanel}  setConfirmRidePopupPanel={setConfirmRidePopupPanel}/>
+      </div>
+      <div ref={confirmRidePopupPanelRef} className='fixed w-full h-screen z-10 bottom-0 -translate-y-full  bg-white  px-3 py-6 pt-12'>
+        <ConfirmRidePopup setConfirmRidePopupPanel={setConfirmRidePopupPanel} setRidePopupPanel={setRidePopupPanel}/>
       </div>
 
-      <div className='flex p-3 mt-6 bg-gray-100 rounded-xl justify-center gap-5 items-start'>
-        <div className='text-center'>
-          <i className='text-3xl mb-2  font-thin ri-timer-2-line'></i>
-          <h5 className='text-lg font-medium'>10.2</h5>
-          <p className='text-sm text-gray-600'>Hours Online</p>
-
-        </div>
-        <div className='text-center'>
-          <i className='text-3xl mb-2  font-thin ri-speed-up-fill'></i>
-          <h5 className='text-lg font-medium'>10.2</h5>
-          <p className='text-sm text-gray-600'>Hours Online</p>
-        </div>
-        <div className='text-center'>
-          <i className='text-3xl mb-2  font-thin ri-booklet-line'></i>
-          <h5 className='text-lg font-medium'>10.2</h5>
-          <p className='text-sm text-gray-600'>Hours Online</p>
-
-        </div>
-      </div >
-       
-      </div>
     </div>
   )
 }
